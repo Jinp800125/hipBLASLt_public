dbcd8c287029a6ed32dc44ac4e0e831ff374612c
Yangwen Huang <yangwen.huang@amd.com>
2023-12-15 07:12:07 +0800

    Fix unable to init when capturing

--- install.sh
+++ install.sh
@@ -661,7 +661,7 @@ pushd .
       install_blis
       popd
 
-      cmake_client_options="${cmake_client_options} -DBUILD_CLIENTS_SAMPLES=ON -DBUILD_CLIENTS_TESTS=ON -DLINK_BLIS=${LINK_BLIS} -DBUILD_CLIENTS_BENCHMARKS=ON"
+      cmake_client_options="${cmake_client_options} -DBUILD_CLIENTS_SAMPLES=ON -DBUILD_CLIENTS_TESTS=ON -DBUILD_CLIENTS_TESTS=ON -DLINK_BLIS=${LINK_BLIS} -DBUILD_CLIENTS_BENCHMARKS=ON"
 
       #
       # Add matrices_dir if exists.
